53e0568345ce51d8214945c21746af21e636e8bf
Akhtar Miah <akhtar.miah@nimbleapproach.com>
2023-08-02 13:17:40 +0100

    hero page updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -21,74 +21,84 @@ export default function Intro() {
     <section
       ref={ref}
       id="home"
-      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+      className="mb-28 max-w-[90%] text-center sm:mb-0 scroll-mt-[100rem]"
     >
-      <Image
-        src={"/bg.png"}
-        width={1200}
-        height={1000}
-        alt=""
-        className="hidden absolute xl:flex xl:max-w-none -z-10 inset-5 -translate-x-1/2 left-1/2"
-      />
-      <div className="flex items-center justify-center"></div>
+      <div className="md:flex items-center justify-center">
+        <div className="w-1/2 items-center justify-center">
+          <div>
+            {/* title */}
 
-      <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-      >
-        <span>
-          Create a society where every individual, regardless of their
-          background, has equal access to education and career opportunities.
-        </span>
-        .
-      </motion.h1>
+            <motion.h1
+              className=" mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0 }}
+            >
+              <span>
+                Create a society where every individual, regardless of their
+                background, has equal access to education and career
+                opportunities.
+              </span>
+              .
+            </motion.h1>
+            <motion.div
+              className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                delay: 0.1,
+              }}
+            >
+              <Link
+                href="#contact"
+                className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+                onClick={() => {
+                  setActiveSection("Contact");
+                  setTimeOfLastClick(Date.now());
+                }}
+              >
+                Join Us{" "}
+                <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+              </Link>
 
-      <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.1,
-        }}
-      >
-        <Link
-          href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-          onClick={() => {
-            setActiveSection("Contact");
-            setTimeOfLastClick(Date.now());
-          }}
-        >
-          Join Us{" "}
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-        </Link>
+              <a
+                className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+                href="/Brochure.pdf"
+                download
+              >
+                Download Brochure{" "}
+                <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+              </a>
 
-        <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Brochure.pdf"
-          download
-        >
-          Download Brochure{" "}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a>
+              <a
+                className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                href="https://linkedin.com"
+                target="_blank"
+              >
+                <BsLinkedin />
+              </a>
 
-        <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
-          target="_blank"
-        >
-          <BsLinkedin />
-        </a>
-
-        <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
-          target="_blank"
-        >
-          <FaGithubSquare />
-        </a>
-      </motion.div>
+              <a
+                className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                href="https://github.com"
+                target="_blank"
+              >
+                <FaGithubSquare />
+              </a>
+            </motion.div>
+          </div>
+        </div>
+        <div className="w-1/2 ">
+          <img src={"/hero.png"} alt="" />
+        </div>
+      </div>
+      <div>
+        <p className="text-center text-xl">
+          We collaborate with{" "}
+          <span className="text-Teal">
+            100+ leading universities and companies
+          </span>
+        </p>
+      </div>
     </section>
   );
 }
